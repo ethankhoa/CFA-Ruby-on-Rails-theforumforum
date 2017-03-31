118ed45c9e6cf0bc61219c432793925a66edddff
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 16:51:43 +1100

    adding login to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+def login=(login)
+  @login = login
+end
+
+def login
+  @login || self.username || self.email
+end
+
 end
