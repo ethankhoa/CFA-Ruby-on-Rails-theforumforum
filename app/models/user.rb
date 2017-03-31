bfb267687422a0cb2e33f519f438e4d0e661c509
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 17:02:42 +1100

    update to allow username for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,17 @@ def login
   @login || self.username || self.email
 end
 
-def self.find_for_database_authentication(warden_conditions)
+def self.find_first_by_auth_conditions(warden_conditions)
   conditions = warden_conditions.dup
   if login = conditions.delete(:login)
-    where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
-  elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-    where(conditions.to_h).first
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  else
+    if conditions[:username].nil?
+      where(conditions).first
+    else
+      where(username: conditions[:username]).first
+    end
   end
-end
 
 validate :validate_username
 
