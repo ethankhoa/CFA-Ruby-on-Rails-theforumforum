7f15584558f54fa95e5785d703cceb9fea919539
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 16:36:55 +1100

    create post controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331053206) do
+ActiveRecord::Schema.define(version: 20170331053528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      default: "", null: false
+    t.text     "content",    default: "", null: false
+    t.integer  "user_id"
+    t.integer  "topic_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title",       default: "", null: false
@@ -41,5 +52,7 @@ ActiveRecord::Schema.define(version: 20170331053206) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "topics"
+  add_foreign_key "posts", "users"
   add_foreign_key "topics", "users"
 end
