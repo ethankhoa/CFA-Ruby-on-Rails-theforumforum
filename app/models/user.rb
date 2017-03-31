d67712971196ede0111ccee018fb827ddc2463df
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 16:56:41 +1100

    update user model for username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,21 @@ def login
   @login || self.username || self.email
 end
 
+def self.find_for_database_authentication(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+  elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+    where(conditions.to_h).first
+  end
+end
+
+validate :validate_username
+
+def validate_username
+  if User.where(email: username).exists?
+    errors.add(:username, :invalid)
+  end
+end
+
 end
