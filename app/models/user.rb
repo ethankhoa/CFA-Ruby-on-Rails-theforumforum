aad5d5446bca6a9572ab76de35a54cd611cee5e9
ethankhoa <ethan.huynh@gmail.com>
2017-03-31 17:21:50 +1100

    setting has_many variables

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :topics
+  has_many :posts
+  has_many :comments
 
 def login=(login)
   @login = login
